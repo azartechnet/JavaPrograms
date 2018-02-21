39f7c8420fd8b243d08d8aac57b0ac4a8cbcc418
azartechnet <azartechnet@gmail.com>
2018-02-21 20:09:43 +0530

    21-02-18

---
+++ ArrayProgram1/src/Demo.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Demo 
+{
+ 
+	
+  int pid[]=new int[5];
+  int n,i;
+  Scanner g=new Scanner(System.in);
+  void get1()
+  {
+	  System.out.println("Enter the value is:\n");
+	  n=g.nextInt();
+	  
+  }
+  void get2()
+  {
+	  for(i=0;i<=n;i++)
+	  {
+		  System.out.println("Enter the Array value is:\n");
+		  pid[i]=g.nextInt();
+		  System.out.println("Array value is:\n"+pid[i]);
+	  }
+  }
+}
+class Foo
+{
+	public static void main(String[] args) 
+	{
+		Demo h=new Demo();
+		h.get1();
+		h.get2();
+
+	}
+
+}
