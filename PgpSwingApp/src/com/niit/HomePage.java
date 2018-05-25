49e37196b4b158f8700f93f199f38f7eeca60b39
azartechnet <azartechnet@gmail.com>
2018-05-25 10:53:39 +0530

    25/5/2018

---
+++ PgpSwingApp/src/com/niit/HomePage.java
@@ -0,0 +1,62 @@
+package com.niit;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class HomePage extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HomePage frame = new HomePage();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public HomePage() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnLoginpage = new JButton("LoginPage");
+		btnLoginpage.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0)
+			{
+				new LoginPage().setVisible(true);
+			}
+		});
+		btnLoginpage.setBounds(170, 49, 89, 23);
+		contentPane.add(btnLoginpage);
+		
+		JButton btnRegister = new JButton("Register");
+		btnRegister.setBounds(170, 117, 89, 23);
+		contentPane.add(btnRegister);
+		
+		JButton btnAdminlogin = new JButton("AdminLogin");
+		btnAdminlogin.setBounds(170, 185, 89, 23);
+		contentPane.add(btnAdminlogin);
+	}
+}
