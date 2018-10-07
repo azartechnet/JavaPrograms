941357edb48b831dc654002dcfeadc2a4292eaba
azartechnet <azartechnet@gmail.com>
2018-10-07 15:33:57 +0530

    07/10/18

---
+++ Prog120/src/Demo.java
@@ -0,0 +1,25 @@
+
+ class Sample
+{
+
+	 int a=10;
+	void get()
+	{
+		a++;
+		System.out.println(""+a);
+		
+	}
+	
+	
+	public static void main(String as[]) {
+		
+		
+		Sample f1=new Sample();
+		Sample f2=new Sample();
+		f1.get();
+		f2.get();
+		//Demo f2=new Demo();
+
+	}
+
+}
