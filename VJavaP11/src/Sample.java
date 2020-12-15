ecb27fb304e60758cd1076771bbc0a8055af2bce
MohamedAzarudeen <azartechnet@gmail.com>
2020-12-15 15:45:05 +0530

    15-12-2020

---
+++ VJavaP11/src/Sample.java
@@ -0,0 +1,18 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		
+		int i,j;
+		for(i=0;i<2;i++)
+		{
+			for(j=0;j<2;j++)
+			{
+				System.out.println("I value is::"+i+"J value is::"+j);
+			}
+		}
+
+	}
+
+}
--- VJavaP9/src/Sample.java
+++ VJavaP9/src/Sample.java
@@ -5,15 +5,26 @@ public class Sample {
 	public static void main(String[] args) {
 
 
-		int i,n;
+		int i,n,j=0,j1=1;
 		Scanner sc1=new Scanner(System.in);
 		System.out.println("Enter the n value is::");
 		n=sc1.nextInt();
-		for(i=1;i<=n;i=+i)
+		for(i=1;i<=n;i++)
 		{
-			System.out.println("I value is::"+i);
+			if(i%2==0)
+			{
+				
+				j=j+i;
+			System.out.println("Even::"+j);
+			}
+			else
+			{
+				j1=j1*i;
+				System.out.println("Odd::"+j1);
+			}
+			
 		}
-		
+		//System.out.println(j);
 
 	}
 
