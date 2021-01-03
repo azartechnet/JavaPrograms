c1e6d1ab3441b8fb0996e5ace24cafd836aa3c6b
MohamedAzarudeen <azartechnet@gmail.com>
2021-01-03 11:48:22 +0530

    03-01-2021

---
+++ VJavaP19/src/Sample.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+class B
+{
+	int productid;
+	Scanner sc=new Scanner(System.in);
+	void get1()
+	{
+		System.out.println("Enter the productid::");
+		productid=sc.nextInt();
+	}
+}
+class D1 extends B
+{
+	int categoryid;
+	void get2()
+	{
+		System.out.println("Enter the categoryId");
+		ac
+		
+	}
+}
+class D2 extends D1
+{
+	
+}
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
