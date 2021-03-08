9005a0379b3236e585c2877fdc82c6b03933de29
MohamedAzarudeen <azartechnet@gmail.com>
2021-03-08 19:09:27 +0530

    08-03-2021

---
+++ EJavaP11/src/Sample.java
@@ -0,0 +1,18 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		int i,j,n=5,m=5;
+		for(i=1;i<=n;i++)
+		{
+			for(j=1;j<=m;j++)
+			{
+				System.out.print(j);
+			}
+			System.out.println("");
+		}
+
+	}
+
+}
