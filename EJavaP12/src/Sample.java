e3c8eb89ffecf7a7ae65433dd3257192e29d0ea5
MohamedAzarudeen <azartechnet@gmail.com>
2021-03-10 17:01:51 +0530

    10-03-2021

---
+++ EJavaP12/src/Sample.java
@@ -0,0 +1,23 @@
+
+public class Sample {
+
+	public static void main(String[] args) {
+		
+		int i,n;
+		i=0;
+		n=5;
+//	    while(i<=n)
+//	    {
+//	    	System.out.println("I value is::"+i);
+//	    	
+//	    	i++;
+//	    }
+		do
+		{
+			System.out.println("I value is::"+i);
+			i++;
+		}while(i<=n);
+
+	}
+
+}
